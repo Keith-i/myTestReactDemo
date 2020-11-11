5419a3c94a745404fe2a4bedc9b5294d580a4641
Keith <754781293@qq.com>
2020-11-11 16:21:06 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import MyCom from './components/myCom'
+import { Button } from 'antd'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <MyCom name="小明" />
         <MyCom name="小红" />
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
